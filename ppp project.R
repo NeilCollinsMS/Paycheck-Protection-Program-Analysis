be40255dbb6a8986797113bd1857b24253d0c486
NeilCollinsMS <68830945+NeilCollinsMS@users.noreply.github.com>
2020-08-25 02:13:12 -0600

    Add files via upload

---
+++ ppp project.R
@@ -0,0 +1,81 @@
+# Data Source: https://www.kaggle.com/susuwatari/ppp-loan-data-paycheck-protection-program
+
+# Project by Neil Collins | 8/18/2020
+
+# This project aims to analyze trends in the Paycheck Protection Program 
+# and to generate predictive models based on demographics to predict the likelihood of receiving a loan.
+
+# Overarching Business Question: What factors played the largest roles in PPP loan distribution?
+
+
+ppp <- read.csv('c://Users/Neil/Desktop/PPP_data_150k_plus.csv')
+
+# Data Exploration
+
+head(ppp) #Taking a look at the first 6 columns of the data
+
+sapply(ppp,class) #finding the data type of each column
+
+summary(ppp)
+
+# Data Cleaning - Utilizes Tidyverse / dplyr
+
+library(tidyverse)
+
+distppp <- select(ppp, LoanRange, City, State, NAICSCode, BusinessType, NonProfit, JobsRetained, Lender) 
+
+# Business name, address, zip code too broad for now. RaceEthnicity, Gender, Veteran too many missing data points, CD is repeat info
+
+# DateApproved left out initially but may be looked at later though all approvals were within an approximate 1 month range before the funds depleted
+
+# Removing extra characters, spaces, coercing numeric
+
+
+library(tm)
+
+distppp$LoanRange <- sub(". ","", distppp$LoanRange)
+
+distppp$LoanRange <- sub(".*-","", distppp$LoanRange)
+
+distppp$LoanRange <- removeWords(distppp$LoanRange," million")
+
+distppp$LoanRange <- sub(",000","",distppp$LoanRange)
+
+distppp$LoanRange <- as.numeric(distppp$LoanRange)
+
+for(i in 1:length(distppp$LoanRange)){
+  
+  if(distppp$LoanRange[i] < 350){
+    distppp$LoanRange[i] <- distppp$LoanRange[i] * 1000000
+  }
+  
+  else if(distppp$LoanRange[i] == 350){
+    distppp$LoanRange[i] <- distppp$LoanRange[i] * 1000
+  }
+  
+}
+
+colnames(distppp)[1] <- "MaxLoan"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
