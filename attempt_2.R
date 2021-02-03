c726bd427b9a08db7dd12abb82aaf5a659e86310
Neil Collins <68830945+NeilCollinsMS@users.noreply.github.com>
2021-02-03 11:08:45 -0700

    Initializing

---
+++ attempt_2.R
@@ -0,0 +1,4 @@
+# Neil Collins - Paycheck Protection Program Analysis Attempt 2 
+
+# Code on how to clean and prepare the distppp dataframe can be found in attempt_1_scrapped.R 
+# I am giving this project a second attempt now that I have revised my model generation methods.
