783761a21f6949b0f6ab2e18849cf0efd678f3ab
Neil Collins <68830945+NeilCollinsMS@users.noreply.github.com>
2021-02-05 16:46:32 -0700

    To include cleaning code

--- attempt_2.R
+++ attempt_2.R
@@ -1,4 +1,5 @@
 # Neil Collins - Paycheck Protection Program Analysis Attempt 2 
 
-# Code on how to clean and prepare the distppp dataframe can be found in attempt_1_scrapped.R 
+# Code on how to clean and prepare the distppp dataframe can be found in attempt_1_scrapped.R. I will be redoing my cleaning process to one-hot encode the target variable.
+# Treating the target loan amounts as numerical data was a fundamental flaw in Attempt 1.
 # I am giving this project a second attempt now that I have revised my model generation methods.
